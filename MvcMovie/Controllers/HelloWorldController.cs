cb50b45cfb22c2588d52e8190ad09235a788b53d
Maksai4ik <mmuzykantov98@gmail.com>
2025-04-30 12:26:47 +0400

    Часть 2

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -1,83 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
-namespace MvcMovie.Controllers
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
 {
-    public class HelloWorldController : Controller
+    // 
+    // GET: /HelloWorld/
+    public string Index()
     {
-        // GET: HelloWorldController
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: HelloWorldController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: HelloWorldController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: HelloWorldController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HelloWorldController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: HelloWorldController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HelloWorldController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: HelloWorldController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        return "Привет мир!";
     }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    
+    public string Welcome(string name, int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+
 }
